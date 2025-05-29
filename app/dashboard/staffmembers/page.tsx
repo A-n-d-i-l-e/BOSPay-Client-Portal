d39a77d6280999d5e22a21e2edf7223881943ad0
A-n-d-i-l-e <89668109+A-n-d-i-l-e@users.noreply.github.com>
2025-05-30 00:50:49 +0200

    .

--- app/dashboard/staffmembers/page.tsx
+++ app/dashboard/staffmembers/page.tsx
@@ -52,6 +52,7 @@ export default function StaffManagementDashboard() {
 
   // Fetch staff members
   useEffect(() => {
+    console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
     const fetchStaff = async () => {
       setLoading(true);
       try {
